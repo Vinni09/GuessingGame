1a3477aeb42fe4c5c38593cc99a22012bd7a6969
Vinni09 <v.flasbart@kabelmail.de>
2023-02-16 14:33:32 +0100

    Added Game finished to output

--- src/org/guess/game/Main.java
+++ src/org/guess/game/Main.java
@@ -51,6 +51,8 @@ public class Main {
                 System.out.println("The number was " + randomNumber + ".");
             }
 
+            System.out.println("Game finished!");
+
         }
 
     }
